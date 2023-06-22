5dbc6994aaac00a421af16f8b1931c168e15460b
euglYA <90235668+euglYA@users.noreply.github.com>
2023-06-23 00:47:58 +0700

    realizations for classes

--- cpu.h
+++ cpu.h
@@ -1,14 +1,67 @@
 
-#ifndef CPU_H
-#define CPU_H
+#ifndef PROCESSOR_H
+#define PROCESSOR_H
 
+#include <QString>
 
-
-
-class processor
-{
-public:
-    processor();
+enum ProcessorType {
+    x86,
+    x64
 };
 
-#endif // CPU_H
+class IProcessor {
+public:
+    virtual ~IProcessor() {};
+    virtual void setProcessor(float cloacks, ProcessorType type, QString model) = 0;
+    virtual QString getProcessorInfo() = 0;
+};
+
+class IntelProcessor : public IProcessor {
+public:
+    ~IntelProcessor() {};
+
+    void setProcessor(double speed, ProcessorType type, QString version) {
+        m_speed = speed;
+        m_type = type;
+        m_version = version;
+    }
+
+    QString getProcessorType() const {
+        return (m_type == ProcessorType::x64 ? " x64 " : " x86 ");
+    }
+
+    QString getProcessorInfo() {
+        return "Intel " + getProcessorType() + "| model: " + m_version + " | speed: " + QString::number(m_speed) + " GHz \n";
+    }
+
+private:
+    double m_speed;
+    ProcessorType m_type;
+    QString m_version;
+};
+
+class AMDProcessor : public IProcessor {
+public:
+    ~AMDProcessor() {};
+
+    void setProcessor(double speed, ProcessorType type, QString version) {
+        m_speed = speed;
+        m_type = type;
+        m_version = version;
+    }
+
+    QString getProcessorType() const {
+        return (m_type == ProcessorType::x64 ? " x64 " : " x86 ");
+    }
+
+    QString getProcessorInfo() {
+        return "AMD " + getProcessorType() + "| model: " + m_version + " | speed: " + QString::number(m_speed) + " GHz \n";
+    }
+
+private:
+    double m_speed;
+    ProcessorType m_type;
+    QString m_version;
+};
+
+#endif // PROCESSOR_H
